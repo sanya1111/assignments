b0245eea44d4ea4d6aa49a590af68f7537a8376f
sanya1 <okay123456@yandex.ru>
2015-09-17 19:04:35 +0300

    formatted source

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,6 +1,5 @@
 package ru.spbau.mit;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -8,99 +7,94 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
+public class StringSetImpl implements StringSet, StreamSerializable {
 
-public class StringSetImpl implements StringSet, StreamSerializable{
-	
-	private class Node{
+	private class Node {
 		static final int ROUTES_SIZE = 200;
-		private Node []routes = new Node[ROUTES_SIZE];
+		private Node[] routes = new Node[ROUTES_SIZE];
 		private int prefCount = 0;
-		boolean end = false;
-		
-		public void push(Character simbol){
+		private boolean end = false;
+
+		public void push(Character simbol) {
 			routes[simbol] = new Node();
 		}
-		
-		public void remove(Character simbol){
+
+		public void remove(Character simbol) {
 			routes[simbol] = null;
 		}
-		
-		public boolean haveNext(Character simbol){
+
+		public boolean haveNext(Character simbol) {
 			return routes[simbol] != null;
 		}
-		
-		
-		public Node next(Character simbol){
+
+		public Node next(Character simbol) {
 			return routes[simbol];
 		}
-		
-		public void incPref(){
+
+		public void incPref() {
 			this.prefCount++;
 		}
-		
-		public void decPref(){
+
+		public void decPref() {
 			this.prefCount--;
 		}
-		
-		public int getPref(){
+
+		public int getPref() {
 			return this.prefCount;
 		}
-		
-		public void markEnd(){
+
+		public void markEnd() {
 			end = true;
 		}
-		public void markNotEnd(){
+
+		public void markNotEnd() {
 			end = false;
 		}
-		
-		public boolean isEnd(){
+
+		public boolean isEnd() {
 			return end;
 		}
-		
-		public void dfs(StringBuilder builder, ArrayList<String> result){
-			if(this.isEnd()){
+
+		public void dfs(StringBuilder builder, ArrayList<String> result) {
+			if (this.isEnd()) {
 				result.add(builder.toString());
 			}
-			
-			for(int i = 0; i < ROUTES_SIZE; i++){
+
+			for (int i = 0; i < ROUTES_SIZE; i++) {
 				Node node = routes[i];
-				if(node != null){
-					builder.append((char)i);
+				if (node != null) {
+					builder.append((char) i);
 					node.dfs(builder, result);
 					builder.deleteCharAt(builder.length() - 1);
 				}
 			}
 		}
-		
-		public void clean(){
+
+		public void clean() {
 			prefCount = 0;
 			end = false;
-			for(int i = 0; i < ROUTES_SIZE; i++){
+			for (int i = 0; i < ROUTES_SIZE; i++) {
 				Node node = routes[i];
-				if(node != null){
+				if (node != null) {
 					node.clean();
-					routes[i] =  null;
+					routes[i] = null;
 				}
 			}
 		}
 	}
-	
+
 	private Node head = new Node();
+
 	@Override
 	public void serialize(OutputStream out) {
 		ArrayList<String> result = new ArrayList<String>();
 		head.dfs(new StringBuilder(), result);
-		
-		try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))){
-			for(String str : result){
+
+		try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+				out))) {
+			for (String str : result) {
 				writer.write(str);
 				writer.newLine();
 			}
@@ -113,8 +107,9 @@ public class StringSetImpl implements StringSet, StreamSerializable{
 	public void deserialize(InputStream in) {
 		head.clean();
 		String str;
-		try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))){
-			while((str = reader.readLine()) != null){
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				in))) {
+			while ((str = reader.readLine()) != null) {
 				add(str);
 			}
 		} catch (IOException e) {
@@ -124,14 +119,14 @@ public class StringSetImpl implements StringSet, StreamSerializable{
 
 	@Override
 	public boolean add(String element) {
-		if(this.contains(element)){
+		if (this.contains(element)) {
 			return false;
 		}
 		Node current = head;
-		for(int i = 0; i < element.length(); i++){
+		for (int i = 0; i < element.length(); i++) {
 			current.incPref();
 			Character simbol = element.charAt(i);
-			if(!current.haveNext(simbol)){
+			if (!current.haveNext(simbol)) {
 				current.push(simbol);
 			}
 			current = current.next(simbol);
@@ -144,9 +139,9 @@ public class StringSetImpl implements StringSet, StreamSerializable{
 	@Override
 	public boolean contains(String element) {
 		Node current = head;
-		for(int i = 0; i < element.length(); i++){
+		for (int i = 0; i < element.length(); i++) {
 			Character simbol = element.charAt(i);
-			if(!current.haveNext(simbol)){
+			if (!current.haveNext(simbol)) {
 				return false;
 			}
 			current = current.next(simbol);
@@ -156,16 +151,16 @@ public class StringSetImpl implements StringSet, StreamSerializable{
 
 	@Override
 	public boolean remove(String element) {
-		if(!this.contains(element)){
+		if (!this.contains(element)) {
 			return false;
 		}
 		Node current = head;
 		current.decPref();
-		for(int i = 0; i < element.length(); i++){
+		for (int i = 0; i < element.length(); i++) {
 			Character simbol = element.charAt(i);
 			Node next = current.next(simbol);
 			next.decPref();
-			if(next.getPref() == 0){
+			if (next.getPref() == 0) {
 				current.remove(simbol);
 			}
 			current = next;
@@ -182,9 +177,9 @@ public class StringSetImpl implements StringSet, StreamSerializable{
 	@Override
 	public int howManyStartsWithPrefix(String prefix) {
 		Node current = head;
-		for(int i = 0; i < prefix.length(); i++){
+		for (int i = 0; i < prefix.length(); i++) {
 			Character simbol = prefix.charAt(i);
-			if(!current.haveNext(simbol)){
+			if (!current.haveNext(simbol)) {
 				return 0;
 			}
 			current = current.next(simbol);
