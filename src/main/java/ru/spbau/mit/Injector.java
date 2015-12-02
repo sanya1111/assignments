c09ed848ce8ae31969a800a7f02e98ccbe090d50
sanya1 <okay123456@yandex.ru>
2015-12-02 17:57:41 +0300

    e(9)

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -3,8 +3,10 @@ package ru.spbau.mit;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class Injector {
@@ -13,7 +15,10 @@ public class Injector {
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
-    private static Object process(Class<?> rootClass, Map<Class<?>, Object> objects, Class<?>[] usedClasses) throws Exception {
+    private static Object process(Class<?> rootClass, Set<Class<?> > parents, Map<Class<?>, Object> objects, Class<?>[] usedClasses) throws Exception {
+        if(parents.contains(rootClass)){
+            throw new InjectionCycleException();
+        }
         if (objects.containsKey(rootClass)) {
             return objects.get(rootClass);
         }
@@ -33,9 +38,10 @@ public class Injector {
             if (candidates.size() > 1) {
                 throw new AmbiguousImplementationException();
             }
-            parametersList.add(process(candidates.get(0), objects, usedClasses));
+            parametersList.add(process(candidates.get(0), parents, objects, usedClasses));
         }
 
+        parents.add(rootClass);
         Object newObject = constructor.newInstance(parametersList.toArray());
         objects.put(rootClass, newObject);
         return newObject;
@@ -49,7 +55,7 @@ public class Injector {
         for (String implementationClassName : implementationClassNames) {
             usedClasses[i++] = Class.forName(implementationClassName);
         }
-        return process(Class.forName(rootClassName), objects, usedClasses);
+        return process(Class.forName(rootClassName),  new HashSet<Class<?>>(), objects, usedClasses);
     }
 
 }
